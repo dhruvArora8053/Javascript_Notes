b9f1e34265b487e387e8b60e963ec85628678c19
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-18 19:22:18 +0530

    strict mode implemented

---
+++ 01 Javascript Fundamentals/fundamentals2.js
@@ -0,0 +1,23 @@
+//////Strict Mode:-
+//strict mode is a special mode that we can activate in javascript which makes it easier for us to write secure javascript code
+
+"use strict";
+//this statement basically has to be the very first statement in the script so if we have any code before this then strict mode will not be activated, comments are allowed because javascript will ignore them.
+
+//strict mode makes it easier for us developers to avoid accidental errors so bascially it avoids us to introduce bugs in our code and that's because of two reasons:
+//1. strict mode forbids us to do certain things
+//2. it will actually create visible errors for us in certain situations in which without strict mode javascript will simply fail silently without letting us know that we did a mistake.
+
+//Examples:-
+let hasDriversLicense = false;
+const passTest = true;
+
+//if (passTest) hasDriverLicense = true; //introducing bug
+if (hasDriversLicense) console.log("I can drive");
+//if here strict mode was not added then we would never get an error of 'hasDriverLicense is not defined'
+
+//now another thing that strict mode does is to introduce a short list of variable names that are reserved for features that might be added to the language a bit later. For Example:
+
+//const interface = "Audio"; //output: unexpected strict mode reserved word
+//const private = 534; //output: unexpected strict mode reserved word
+//const if= 23 //output: unexpected toke 'if'
