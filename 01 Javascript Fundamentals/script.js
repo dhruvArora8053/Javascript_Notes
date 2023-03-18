8f133f4f6dff9a4ad6af766b1e5421599cb2408d
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-18 17:33:04 +0530

    let, const and var

--- 01 Javascript Fundamentals/script.js
+++ 01 Javascript Fundamentals/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+///// Variables:-
 //Box analogy to variable
 //1. let,const,var = box
 //2. varName = boxLabel
@@ -15,7 +15,35 @@
 //variable conventions:
 //1. camelcase
 //2. constants start with uppercase such as PI
-//3. variable names should be descriptive 
+//3. variable names should be descriptive
 
+////Data Types:-
+//seven primitive data types:
+//1. number          let age=23;
+//2. string          let firstName='jonas';
+//3. boolean         let fullAge= true;
+//4. undefined       let children; //notAValue
+//5. null            'empty value'
+//6. symbol          not useful for now
+//7. big int
 
+//javascript has a dynamic typing: no need to give datatypes and we can change variable from number to string with no problem at all.
 
+console.log(typeof null); //output: object
+//this makes no sense it is a bug in javascript language and they didn't remove it because of the legacy reasons
+
+//////////let, const and var:-
+//let: mutation possible
+
+//const: mutation not possible 
+//       need to assign value while declaring
+//       try to always use const 
+
+//var: mutation possible
+
+firstName = 'schmedtmann' //totally legal but not a good idea
+console.log(firstName);
+//but javscript will create a property on the global object.
+
+////////Basic Operators:
+console.log(2**3);
