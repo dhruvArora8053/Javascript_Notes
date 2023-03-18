2b40f57d891de19ae705de6d86f4b96a30017513
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-18 16:19:33 +0530

    variables noted

---
+++ 01 Javascript Fundamentals/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//Box analogy to variable
+//1. let,const,var = box
+//2. varName = boxLabel
+//3. value = boxContent
+
+//variable declaration rules:
+//1. camelcase
+//2. cannot start with numbers
+//3. no use of signs other than $ and _
+//4. cannot use reserve keywords ex: new etc.
+//5. 'name' kind of reserve keyword so do not use
+
+//variable conventions:
+//1. camelcase
+//2. constants start with uppercase such as PI
+//3. variable names should be descriptive 
+
+
+
