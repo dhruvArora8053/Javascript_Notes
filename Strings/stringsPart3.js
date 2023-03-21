dc07bea1c1595b07d9d8775af8e576df024891eb
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-21 16:58:40 +0530

    join and split method implemented

--- Strings/stringsPart3.js
+++ Strings/stringsPart3.js
@@ -1,2 +1,25 @@
 "use strict";
 
+//Split Method:
+console.log("a+very+nice+string".split("+"));
+//here it will split up this string by the + sign and it will then store the results into elements of a new array.
+
+console.log("Jonas Schmedtmann".split(" "));
+
+//using destructuring:
+const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
+console.log(firstName, lastName);
+
+//Join method:
+//opposite of split method:
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+
+console.log(newName);
+
+//Example: how to capitalize a name:
+const capitalizeName = function (name) {
+    
+};
+
+capitalizeName("jessica ann smith davis");
+capitalizeName('jonas schmedtmann')
