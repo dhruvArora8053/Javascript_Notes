c51214d21f804502999d7a59db7dba5b26cbfa6e
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-23 16:04:38 +0530

    spread operator implemented

--- Modern Operators/spreadOperator.js
+++ Modern Operators/spreadOperator.js
@@ -37,6 +37,12 @@ const restaurant = {
       `Order recieved! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be dilivered to ${address} at ${time}`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 };
 
 //Spread Operator:
@@ -73,3 +79,39 @@ const mainMenuCopy = [...restaurant.mainMenu];
 //join two arrays together:
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 console.log(menu);
+
+//The spread operator works on arrays as well as on so-called iterables
+//What is an iterable?
+//iterables are things like all [arrays, strings, maps, or sets] but not objects, so basically most of the built-in data structures in javascript are now iterables but except objects.
+
+//since strings are also iterables that means that we can use the spread operator on a sting as well:
+const str = "Jonas";
+const letters = [...str, " ", "S."];
+console.log(letters);
+console.log(...str);
+// console.log(`${...str} Schmedtmann`); //output: error
+//this won't work and that's because this is not a place that expects multiple values separated by comma, because multiple values separated by comma are usually only expected when we pass arguments into a function or when we build a new array.
+
+//Note:- now just keep in mind that we can still only use the spread operator when building an array or when we pass values into a function
+
+//Practical example:-
+//write a function that accepts multiple arguments and then use the spred operator to actually pass those arguments:
+// const ingredients = [
+//   prompt("Let's make pata! Ingredient 1?"),
+//   prompt("Let's make pata! Ingredient 2?"),
+//   prompt("Let's make pata! Ingredient 3?"),
+// ];
+// console.log(ingredients);
+
+// restaurant.orderPasta(...ingredients);
+
+//Since ES 2018, the spread operator actually works on objects even though objects are not iterables
+const newRestaurant = { foundedIn: 1998, ...restaurant, founder: "Guiseppe" };
+console.log(newRestaurant);
+//this will basically copy all the properties of the restaurant into this new object and then we can add anything that we want and ofcorse here order does not matter as always so this spread here doesn't have to first one
+
+//creating shallow copies of objects:
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Ristorante Roma";
+console.log(restaurantCopy.name); //output: Ristorante Roma
+console.log(restaurant.name); //output: Classico Italiano
