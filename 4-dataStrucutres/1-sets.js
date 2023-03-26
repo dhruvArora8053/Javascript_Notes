cbc80c85bd9f4e4a36308000a1d52c3df9e5c944
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-26 22:54:21 +0530

    implementing sets

--- 4-dataStrucutres/1-sets.js
+++ 4-dataStrucutres/1-sets.js
@@ -54,5 +54,35 @@ const restaurant = {
   },
 };
 
+//Traditional javascript Data Structures:
+//1. Arrays
+//2. Objects
+
+//ES6:
+//1. Sets
+//2. Maps
+
+//Sets:-
+//a set is basically just a collection of unique values so that means that a set can never have any duplicates and that property makes them useful in certain situations
+
+const ordersSet = new Set([
+  "Pasta",
+  "Pizza",
+  "Pizza",
+  "Risotto",
+  "Pasta",
+  "Pizza",
+]);
+//all are strings here but ofcourse there could be different types in here so set can hold mixed data types
+console.log(ordersSet);
+//all the duplicates are gone, they are a bit simlilar to arrays(no key value pairs), it's just a bunch of values grouped together in this case into a set and just like arrays sets are also iterables
+
+//set is still very different from an array
+//1. because it's elements are unique
+//2. the order of elements in set is irrelevant
+
+console.log(new Set("Jonas")); //because strings are also iterables
+
+console.log(ordersSet.size); //output: 3
 
 
