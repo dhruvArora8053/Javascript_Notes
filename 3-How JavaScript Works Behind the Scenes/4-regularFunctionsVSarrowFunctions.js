ee92372885ca4e33b681d29c3e51459a41ac140c
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-30 18:04:40 +0530

    implementing this keyword

---
+++ 3-How JavaScript Works Behind the Scenes/4-regularFunctionsVSarrowFunctions.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const jonas = {
+  firstName: "Jonas",
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+
+jonas.greet(); //output: Hey undefined
+//the reason for this output is as we previously mentioned which is the fact that an arrow function does not get it's own this keyword, it will simply use the this keyword from it's surroundings so in other words it's parent this keyword and the parent scope of this greet method is the global scope so it means it has window object
+//Note:- jonas object is not actually a code block so you might think that like the curly braces denoting the code block here and ought to create it's own scope but it doesn't. So this is not a code block, it is an object literal so it's just a way that we literally define objects so all of the jonas object is still in global scope
+
+console.log(this.firstName); //output: undefined
+//when there is certain property doesn't exist on an object we do not get an error instead we get undefined and this behavior can become pretty dangerous incase we use var to declare variables:
+
+var firstName = "Matilda";
+//remember variables declared with var actually create properties on the global object
+
+const jonas1 = {
+  firstName: "Jonas",
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+
+
+
