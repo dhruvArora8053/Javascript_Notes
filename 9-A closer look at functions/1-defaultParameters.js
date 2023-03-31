3d5fad6b859993bc7ac86b0ba7c8706d7d0e122a
Dhruv Arora <dhruvarora.code@gmail.com>
2023-03-31 21:24:09 +0530

    default parameter implemented

--- 9-A closer look at functions/1-defaultParameters.js
+++ 9-A closer look at functions/1-defaultParameters.js
@@ -1,2 +1,36 @@
-'use strict';
+"use strict";
 
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers //<--modern way //and they can set to any expression and even we can even assign the old parameters that are before them
+) {
+  //To set default values:
+  //Traditional Way-
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking("LH123");
+//her numPassengers and price are undefined
+
+createBooking("LH123", 2, 800);
+//this will overwrite the default values
+
+createBooking("Lh123", 2);
+createBooking("Lh123", 5);
+//above price is getting calculated in the default parameter
+
+//if we want to leave some argument empty:
+createBooking("LH123", undefined, 1000);
+//this works because setting the parameter to undefined is the same thing as not even setting it
