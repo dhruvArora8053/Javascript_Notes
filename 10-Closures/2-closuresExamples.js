5bc37dbe3af21f1217e7efe7cd58b5de1cddf4eb
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-02 21:10:42 +0530

    closures examples implemented

--- 10-Closures/2-closuresExamples.js
+++ 10-Closures/2-closuresExamples.js
@@ -32,6 +32,7 @@ console.dir(f);
 //Example 2:
 const boardPassengers = function (passangers, wait) {
   const perGroup = passangers / 3;
+  //if we comment above statement then callback would use the global variable perGroup
 
   setTimeout(function () {
     console.log(`We are now boarding all ${passangers} passengers`);
@@ -41,8 +42,15 @@ const boardPassengers = function (passangers, wait) {
   console.log(`Will start boarding in ${wait} seconds`);
 };
 
+// boardPassengers(180, 3);
+//here boardPassengers function has already been executed but still setTimeout was able to use perGroup,n and wait variables because of closure
+
+//Note:-the closure ofcourse also includes the arguments and that's because they are really just a local variables in the function.
+
+//let's also establish that the closure does infact have priority over the scope chain:
+const perGroup = 1000;
 boardPassengers(180, 3);
-//here boardPassengers function has already been executed but still setTimeout was able to use perGroup and n variable because of closure
+//if the scope chain had priority over the closure then the callback function inside of setTimeout would indeed use above global variable because we can imagine callback function basically being executed in the global scope so if it wasn't for the closure then it would have definitely used global perGroup variable
 
 //setTimeout() is a function which will execute the callback function after given milli seconds:
 // setTimeout(() => {
