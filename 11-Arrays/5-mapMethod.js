db4d531fd798a0413164f3c17844322b8a13b153
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-03 18:41:24 +0530

    imlementing map method

--- 11-Arrays/5-mapMethod.js
+++ 11-Arrays/5-mapMethod.js
@@ -24,3 +24,14 @@ for (const mov of movements) {
 console.log(arr);
 
 //so here in above map method we use a function to solve the problem of creating a new array while here in forOf we simply loop over one array and then manually create a new one so these two are completely different philosphies or we can also say paradigms, map method using a functional programming which is more preffered because in modern javascript there is definitely a push going on in the direction of fucntional programming and therefore in modern javascript map method is way to go then forOf for this type of computation.
+
+//another example:
+const movementsDescriptions = movements.map((mov, i, arr) => {
+  if (mov > 0) {
+    return `Movement ${i + 1}: You deposited ${mov}`;
+  } else return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+});
+
+console.log(movementsDescriptions);
+
+//Note:- It's completely acceptable to have two return statements or even more in the same fucntion as long as only one of them is executed.
