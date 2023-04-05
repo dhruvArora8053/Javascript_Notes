ef65df333a155116fb993886686783c0b2d1d5b7
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-05 19:56:18 +0530

    every method implemented

--- 11-Arrays/11-some&everyMethods.js
+++ 11-Arrays/11-some&everyMethods.js
@@ -20,3 +20,17 @@ const anyDeposits1 = movements.some((mov) => mov > 5000);
 console.log(anyDeposits1); //false
 
 //Half part: bankist app: request a loan functionality
+
+//every method: if each and every element satisfies the condition the it will return true otherwise false
+console.log(movements.every((mov) => mov > 0)); //false
+
+const movements1 = [430, 1000, 700, 50, 90];
+console.log(movements1.every((mov) => mov > 0)); //true
+
+//So up until this point we have always written the callback function directly as an argument into our array methods however, we could also write the function seprately and then pass the fucntion as a callback:
+const deposit = (mov) => mov > 0;
+console.log(movements.some(deposit)); //true
+
+//and now we could reuse the same deposit function for all kinds of different methods that require callbacks with a true/false condition:
+console.log(movements.every(deposit)); //false
+console.log(movements.filter(deposit));
