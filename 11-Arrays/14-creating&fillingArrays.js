434af358766d49798fb4f075d68753155eee759c
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-06 14:11:13 +0530

    strings recalled

---
+++ 11-Arrays/14-creating&fillingArrays.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//already learnt:
+console.log([1, 2, 3, 4, 5, 6, 7]);
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+//new ways:
+const x = new Array(7);
+console.log(x);
+//it creates a new array with seven empty elements
