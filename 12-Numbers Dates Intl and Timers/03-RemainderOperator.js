3c199d640d0ead752059f88e7f6ba8316cff1f77
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-07 20:33:56 +0530

    remainder operator implemented

---
+++ 12-Numbers Dates Intl and Timers/03-RemainderOperator.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log(5 % 2); //1
+console.log(5 / 2); //2.5
+
+console.log(8 % 3); //2
+console.log(8 / 3);
+
+const isEven = function (n) {
+  n % 2 === 0 ? console.log("even") : console.log("odd");
+};
+
+isEven(67);
+
+//Half Part: Bankist App
