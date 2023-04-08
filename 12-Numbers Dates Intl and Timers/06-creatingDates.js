40098fcb32751d97a876a38042bc2999e4d5e30b
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-08 20:55:06 +0530

    creating dates recalled

--- 12-Numbers Dates Intl and Timers/06-creatingDates.js
+++ 12-Numbers Dates Intl and Timers/06-creatingDates.js
@@ -95,6 +95,6 @@ console.log(Date.now());
 future.setFullYear(2040);
 console.log(future);
 //now the 'future' date got mutated to Nov 19 2040
-//and there also exists setMonh, setDate, and so on and so forth
+//and there also exists setMonth, setDate, and so on and so forth
 
 console.log(future.set);
