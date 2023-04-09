ca0a23a24f93fec218829dc005fda6e5c47ff00d
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-09 22:15:40 +0530

    Intl dates implementeed

--- 12-Numbers Dates Intl and Timers/Bankist App/script.js
+++ 12-Numbers Dates Intl and Timers/Bankist App/script.js
@@ -89,7 +89,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 //Now we could simply start writing our code out here in the global context however, that is not a good practice so whenever we do something like creating a feature, it's always best to create a function:
 
-const formatMovementDate = function (date) {
+const formatMovementDate = function (date, locale) {
   const calcdaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
@@ -100,13 +100,51 @@ const formatMovementDate = function (date) {
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
   else {
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0); //zero based
-    const year = date.getFullYear();
+    // const day = `${date.getDate()}`.padStart(2, 0);
+    // const month = `${date.getMonth() + 1}`.padStart(2, 0); //zero based
+    // const year = date.getFullYear();
 
-    return `${day}/${month}/${year}`;
+    // return `${day}/${month}/${year}`;
+
+    return new Intl.DateTimeFormat(locale).format(date);
+    //in this case we don't want any options object because here we don't need the hours or the minutes all we want is to simply display the date as simple as possible and in that case we don't even need any options
   }
 };
+
+// //Internationalizing Dates API:-
+// const now = new Date();
+
+// //we need to pass inside the Intl.DateTimeFormat function is a so called locale string and this locale is usually the 'language and then dash the country' after passing the locale string all of this will create the so-called formatter for the particular language and the country so all of this create a new formatter and then on that foramtter we can call .format and then here we actually pass in the date that we want to format
+// labelDate.textContent = new Intl.DateTimeFormat('en-US').format(now); //As of 4/9/2023
+// //now you see the date is actually formatted just like it usually formatted in the US with the month first and then the day, let's try UK:
+// labelDate.textContent = new Intl.DateTimeFormat('en-UK').format(now); //As of 09/04/2023
+// //now you see date formatting is different, so we have the day first then the month and it also has leading zero, let's try arabic for syria:
+// labelDate.textContent = new Intl.DateTimeFormat('ar-SY').format(now); //As of ٩/٤/٢٠٢٣
+
+// //And to get these different codes: Google, ISO language code table: lingoes.net
+
+// //We can actually take it to the next level and add some options to also customize these dates a little bit:
+// const options = {
+//   hour: 'numeric',
+//   minute: 'numeric',
+//   day: 'numeric',
+//   month: 'long',
+//   year: '2-digit',
+//   weekday: 'short',
+// };
+
+// labelDate.textContent = new Intl.DateTimeFormat('en-US', options).format(now);
+
+// //now let's see for portugal:
+// labelDate.textContent = new Intl.DateTimeFormat('pt-PT', options).format(now);
+
+// //let's get locality from the user's browser:
+// const locale = navigator.language;
+// console.log(locale);
+
+//Note:- Check Intl API for other operations in MDN Documentation
+
+// labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(now);
 
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
@@ -127,7 +165,7 @@ const displayMovements = function (acc, sort = false) {
 
     //acc.movements[i] is gonna be a nicely formatted time string and we can use that string to create a new date object and we need that object so that then from there we can call our usual methods to get the date, month and a year so that's the reason why we need to convert these strings back into a javascript object because only then we can actually work with that data
 
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
 
     const html = `
     <div class="movements__row">
@@ -227,12 +265,26 @@ btnLogin.addEventListener('click', function (e) {
 
     //Create current Date and Time:
     const now = new Date();
-    const day = `${now.getDate()}`.padStart(2, 0);
-    const month = `${now.getMonth() + 1}`.padStart(2, 0); //zero based
-    const year = now.getFullYear();
-    const hour = `${now.getHours()}`.padStart(2, 0);
-    const min = `${now.getMinutes()}`.padStart(2, 0);
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+    // const day = `${now.getDate()}`.padStart(2, 0);
+    // const month = `${now.getMonth() + 1}`.padStart(2, 0); //zero based
+    // const year = now.getFullYear();
+    // const hour = `${now.getHours()}`.padStart(2, 0);
+    // const min = `${now.getMinutes()}`.padStart(2, 0);
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'numeric',
+      year: '2-digit',
+      // weekday: 'short',
+    };
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
 
     //Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
