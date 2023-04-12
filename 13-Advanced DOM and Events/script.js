60c2e8a72ba27ed89a43c47ddc8892271fe8dfe2
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-12 13:45:53 +0530

    element created and inserted

--- 13-Advanced DOM and Events/script.js
+++ 13-Advanced DOM and Events/script.js
@@ -62,3 +62,33 @@ console.log(document.getElementsByClassName('btn'));
 //this one also returns the html collection
 
 //Creating and Inserting Elements:-
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics';
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close--cookie">Got it!</button>';
+//this will return a dom element that we can save somewhere now this element is not yet anywhere in our DOM all this is a dom object that we can now use to do something on it but it is not yet in the dom itself so it's nowhere to be found in our webpage, if we want it on the page then we need to manually insert it to the page:
+//Inserting Element:
+const header = document.querySelector('.header');
+header.prepend(message);
+//now the message is really in our dom, we just inserted this element into our html so right into our dom so prepending basically adds the element as the first child of the parent element but we can also add it as the last child:
+
+header.append(message);
+//now what we see here is that the element was actually only insert at once, that's because message element is now indeed a live element living in the dom and so therefore it cannot be at multiple palces at the same time, it's just like a person that also cannot be at two place simultaneously
+
+//but now what if we actually wanted to insert multiple copies of the same element?
+//well in that case we actually would have to first copy the first element:
+// header.append(message.cloneNode(true));
+//here the true means all the child elements will also be copied
+
+//Two more methods to insert element:
+header.before(message);
+//this one will insert a message element before the header element
+
+header.after(message);
+//this one will insert a message element after the header element
+
+//Delete Elements:-
+//as we click the cookie the button, remove the message element:
