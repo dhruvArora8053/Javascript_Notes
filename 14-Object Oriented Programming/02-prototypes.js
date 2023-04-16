2a65206bbca6e6b5f10657cce177ccc5f5d450aa
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-16 20:03:44 +0530

    implementing prototypes

--- 14-Object Oriented Programming/02-prototypes.js
+++ 14-Object Oriented Programming/02-prototypes.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const Person = function (firstName, birthYear) {
-  console.log(this);
+  //   console.log(this);
   //indeed here we got the empty object and the browser console is actually already telling us that it's basically here of the type 'Person'
 
   //And now let's use this knowledge to our advantage becuse we already that in the end of this function the this keyword will basically be returned and so whatever we add to that empty object will then be returned from the function and that returned object is gonna be the object that we are trying to build here:
@@ -10,26 +10,27 @@ const Person = function (firstName, birthYear) {
   //here we gave same name to the properties as of parameters ofcourse we can set other names but it's just a convention
 
   //Never do this
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  //   this.calcAge = function () {
+  //     // console.log(2037 - this.birthYear);
+  //   };
   //we should never create a method inside of a constructor function, it is because:
   //image we were gonna create a hundered or thousands or even tens of thousands of person objects using this constructor function then what would happen is that each of these objects would carry around this function here so if we had a thousand objects we would essentially create a thousand copies of this function and so that would be terrible of the performance of our code but instead to solve this problem we are gonna use prototypes and prototype inheritance
 };
 
 const jonas = new Person("Jonas", 1991);
-console.log(jonas);
+
+// console.log(jonas);
 
 //Now we can use this constructor function to create as many different objects as we want:
 const matilda = new Person("Matilda", 2017);
 const jack = new Person("Jack", 2001);
 
-console.log(matilda, jack);
+// console.log(matilda, jack);
 
 //And this is a bit like the analogy from before where the above constructor function is now the bluepring for a house and then each of these objects that we create through that function will be the actual house in the real world
 
 //Now remeber from one of the previous lectures that in classical OOP, an object created from a class is called an instance, now we didn't technically create a class here because as we discussed before javascript doesn't really have classes in the sense of traditional OOP however, we did created 3 objects from a constructor function. And constructor functions have been used since the beginning of javascript to kind of simulate classes and so therefore we can still say that jonas here is an instance of a Person and the same goes for Matilda and Jack and infact there is even an operator that we can use to test for that:
-console.log(jonas instanceof Person); //true
+// console.log(jonas instanceof Person); //true
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Start from here:
@@ -42,3 +43,21 @@ console.log(jonas instanceof Person); //true
 Person.prototype.calcAge = function () {
   console.log(2036 - this.birthYear);
 };
+
+console.log(Person.prototype);
+
+//now we would be able to do this:
+jonas.calcAge();
+//so here we did able to use calcAge method on the jonas object even though it is not really on the object itself so if check jonas here:
+console.log(jonas);
+//you see that it contains ofcourse the birthYear and the firstName butt it does not contain calcAge method but still we have access to it because of prototypal inheritance now let's check for matilda and jack:
+matilda.calcAge();
+jack.calcAge();
+
+//And now it also solves the previous problem of getting copied each method to every object, because now there exists only one copy of calcAge function and now all of the objects that are created using the 'Person' constructor function can basically reuse this function on themselves and so the this keyword ofcorse in each of them is as always set to the object that is calling the method
+
+//But how this all works?
+//Well, it works because any object always has access to the methods and properties from it's prototype and the prototype of jonas and matilda is Person.prototype and we can actually confirm that becase each object has a special property:
+console.log(jonas.__proto__);
+//so the prototype of the jonas object is essentially the prototype property of the constructor function:
+console.log(jonas.__proto__ === Person.prototype); //true
