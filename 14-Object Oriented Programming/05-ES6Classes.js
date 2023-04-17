a1fa34e4be37befb1614cdb11ec332fbd36e6045
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-17 18:29:38 +0530

    classes added

--- 14-Object Oriented Programming/05-ES6Classes.js
+++ 14-Object Oriented Programming/05-ES6Classes.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//Now as we mentioned earlier classes in javascript do not work like traditional classes in other languages like Java or C++ so instead classes in javascript are just synthetic sugar over what we learned in these last sections. So they still implement prototypal inheritance behing the scenes but with a syntax that makes more sense to people coming from othe programming languages and so that was basically the goal of adding classes to javascript:
+//Now as we mentioned earlier classes in javascript do not work like traditional classes in other languages like Java or C++ so instead classes in javascript are just synthetic sugar over what we learned in these last sections. So they still implement prototypal inheritance behind the scenes but with a syntax that makes more sense to people coming from othe programming languages and so that was basically the goal of adding classes to javascript:
 
 //class expression
 const PersonCl = class{
