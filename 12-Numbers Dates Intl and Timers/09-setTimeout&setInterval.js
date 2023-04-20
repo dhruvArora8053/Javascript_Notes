14e9c6d570e6fb8b7d39df0c6471292d62937dad
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-20 16:36:46 +0530

    setTimeout and setInterval recalled

--- 12-Numbers Dates Intl and Timers/09-setTimeout&setInterval.js
+++ 12-Numbers Dates Intl and Timers/09-setTimeout&setInterval.js
@@ -10,8 +10,8 @@ const pizzaTimer = setTimeout(
   2000,
   ...ingredients
 );
-//we do not call this function ourselves we simply pass in the funciton as an argument to setTimeout and it is then setTimeout function who will the callback function in the future
-//passing arguments inside of a callback funciton is not possible without a function name so after milliseconds we can provide those arguments to our callback function
+//we do not call this function ourselves we simply pass in the funciton as an argument to setTimeout and it is then setTimeout function who will call the callback function in the future
+//passing arguments inside of a callback function is not possible without a function name so after milliseconds we can provide those arguments to our callback function
 
 console.log("Waiting...");
 //after setTimeout function, the other statements will be immediately exectued, it won't wait for the setTimeout function to be completely executed and this mechanism is called Asynchrounous Javascript
