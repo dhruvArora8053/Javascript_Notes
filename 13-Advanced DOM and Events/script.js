161f3a95604722d45182f238ee54cf4a6ba7bd74
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-22 19:24:59 +0530

    event delegation implemented

--- 13-Advanced DOM and Events/script.js
+++ 13-Advanced DOM and Events/script.js
@@ -110,10 +110,14 @@ btnScrollTo.addEventListener('click', function (e) {
 //1. Add event listener to common parent element
 //2. Determine what element originated the event
 
-document.querySelector('.nav__links').addEventListener('click', function(e){
+document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
-  
-})
+  console.log(e.target);
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
 // document.querySelector('.nav__links').addEventListener('click', function (e) {
 //   //figuring where the event happend
