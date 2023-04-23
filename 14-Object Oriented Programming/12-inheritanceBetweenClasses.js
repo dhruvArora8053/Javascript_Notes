0ba4fb40e99abf4be8bbbfcfa01711e5d5d23393
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-23 20:02:01 +0530

    implemented inheritance between classes

--- 14-Object Oriented Programming/12-inheritanceBetweenClasses.js
+++ 14-Object Oriented Programming/12-inheritanceBetweenClasses.js
@@ -25,6 +25,20 @@ class StudentCl extends PersonCl {
     this.course = course;
     //and declaring this statement is not mandatory
   }
+
+  introduce() {
+    console.log(`My name is ${this.firstName} and I study ${this.course}`);
+  }
+
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      }`
+    );
+  }
 }
 
 //with constructor method
@@ -36,3 +50,12 @@ class StudentCl1 extends PersonCl {}
 const martha1 = new StudentCl1("Martha", 2012, "Computer Science");
 console.log(martha1);
 //this was just to demonstrate that if you do not need any new properties then you don't even need to bother writing a constructor method in a child class
+
+martha.introduce();
+martha.calcAge();
+console.log(martha);
+//so you see that indeed in a prototype of Martha we now have the introduce method but now in the prototype of prototype we indeed have the calcAge method and so this proves us that the prototype chain was indeed setup automatically by extends keyword
+
+//overriding method
+martha.calcAge();
+//and so now ideed this new calcAge method overrode the one that was already there in the prototype chain and again that's because this new calcAge method here appears first in the prototype chain. And we can also say that this calcAge method here is shadowing the parent's class calcAge method.
