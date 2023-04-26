d92897db7a2df974d3916b52eab7b34e893a2fc5
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-26 22:07:27 +0530

    my example added

--- 14-Object Oriented Programming/13-inheritanceBetweenClasses_Object.create.js
+++ 14-Object Oriented Programming/13-inheritanceBetweenClasses_Object.create.js
@@ -16,7 +16,7 @@ steven.init("Steven", 2012);
 console.log(steven);
 steven.calcAge();
 
-//So PersonProto above used to be the prototype of all the new person objects but now bascially we want to add another prototype in the middle fo the chain so between PersonProt and the object and so what we are gonna do is to make student inherit directly from person
+//So PersonProto above used to be the prototype of all the new person objects but now bascially we want to add another prototype in the middle of the chain so between PersonProto and the object and so what we are gonna do is to make student inherit directly from person
 
 const StudentProto = Object.create(PersonProto);
 //and now we can use this StudentProto to create new students
@@ -40,3 +40,35 @@ jay.introduce();
 jay.calcAge();
 
 //So in this version we don't even worry about constructors anymore and also not about prototype properties and not about the new operator, so it's really just object linked to other objects and it's all really simple and beautiful.
+
+const FatherProto = {
+  init(firstName, car) {
+    this.firstName = firstName;
+    this.car = car;
+  },
+
+  onlyCar() {
+    console.log(`I have only ${this.car}`);
+  },
+};
+
+const jack = Object.create(FatherProto);
+jack.init("Jack", "Mercedes");
+console.log(jack);
+
+const SonProto = Object.create(FatherProto);
+
+SonProto.init = function (firstName, car, bike) {
+  FatherProto.init.call(this, firstName, car);
+  this.bike = bike;
+};
+
+SonProto.carBike = function () {
+  console.log(`I have ${this.car} and ${this.bike}`);
+};
+
+const john = Object.create(SonProto);
+john.init("John", "BMW", "Duke");
+console.log(john);
+john.onlyCar();
+john.carBike();
