7aaae7e5ae4c21d8c1b39d2a8b7b6eceb3bf928c
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-27 14:08:41 +0530

    last challenge implemented

--- 14-Object Oriented Programming/18-challenge.js
+++ 14-Object Oriented Programming/18-challenge.js
@@ -10,4 +10,63 @@
 // class. Then experiment with chaining!
 // Test data:
 // § Data car 1: 'Rivian' going at 120 km/h, with a charge of 23%
-// GOOD LUCK 
+// GOOD LUCK
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`New Speed: ${this.speed}`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`New Speed: ${this.speed}`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+}
+
+const car1 = new CarCl("BMW", 120);
+const car2 = new CarCl("Mercedes", 95);
+const car3 = new CarCl("Ford", 120);
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    console.log(
+      `${this.make} going at ${this.speed}km/h with a charge of ${
+        this.#charge
+      }%`
+    );
+
+    return this;
+  }
+}
+
+const rivian = new EVCl("Rivian", 120, 23);
+
+rivian.brake();
+rivian.chargeBattery(90);
+console.log(rivian);
+rivian.accelerate();
+rivian.chargeBattery(40).accelerate().accelerate();
