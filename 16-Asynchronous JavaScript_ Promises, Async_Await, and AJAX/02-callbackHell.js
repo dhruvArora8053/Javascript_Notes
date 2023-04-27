5306da9726f829a93ec317fb9f4073194954c8d0
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-27 21:54:24 +0530

    function added

--- 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/02-callbackHell.js
+++ 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/02-callbackHell.js
@@ -1,1 +1,32 @@
 'use strict';
+
+//Let's create a sequence of ajax calls so that the second one runs only after the first one has finished:
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+
+  request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = `<article class="country">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+        <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
+      </div>
+    </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
