ba419249017eaefdd408e3fdc30f292b5b4de773
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-28 21:04:26 +0530

    implementing chaining among p;romises

---
+++ 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/05-chainingPromises.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+  const html = `<article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+        <p class="country__row"><span>💰</span>${data.currencies.name}</p>
+      </div>
+    </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+//////////////////////////////////////////////////
+//Start from here:-
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('portugal');
+
+//Actually we already have a small chain of promises because of above json function so these above two thens called in sequence are basically already a small chain but anyway we will now take chaining to a new level:
