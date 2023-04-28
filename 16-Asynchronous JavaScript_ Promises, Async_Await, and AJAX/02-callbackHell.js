334fc4c9b549344becc32140d63602ecb26b67aa
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-28 16:57:00 +0530

    sets recalled

--- 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/02-callbackHell.js
+++ 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/02-callbackHell.js
@@ -1,7 +1,31 @@
 'use strict';
 
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+/////////////////////////////////////////////////
 //Let's create a sequence of ajax calls so that the second one runs only after the first one has finished:
-const getCountryData = function (country) {
+
+const renderCountry = function (data) {
+  const html = `<article class="country">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.common}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}M people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
+      <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
+    </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  //AJAX call country 1
   const request = new XMLHttpRequest();
 
   request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
@@ -12,21 +36,19 @@ const getCountryData = function (country) {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
 
-    const html = `<article class="country">
-      <img class="country__img" src="${data.flags.png}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name.common}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)}M people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
-        <p class="country__row"><span>💰</span>${data.currencies.EUR.name}</p>
-      </div>
-    </article>`;
+    //Render Country 1
+    renderCountry(data);
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    //Get Neighbour Country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    //AJAX call country 2
+    const request = new XMLHttpRequest();
+    request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
+    request.send();
   });
 };
 
+getCountryAndNeighbour('portugal');
