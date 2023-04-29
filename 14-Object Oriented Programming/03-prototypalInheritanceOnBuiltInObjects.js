742df765dec80dfa54d668c64a42383346d71c0d
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-29 17:24:12 +0530

    comments removed

--- 14-Object Oriented Programming/03-prototypalInheritanceOnBuiltInObjects.js
+++ 14-Object Oriented Programming/03-prototypalInheritanceOnBuiltInObjects.js
@@ -1,38 +1,13 @@
 "use strict";
 
-"use strict";
-
 const Person = function (firstName, birthYear) {
-  //   console.log(this);
-  //indeed here we got the empty object and the browser console is actually already telling us that it's basically here of the type 'Person'
-
-  //And now let's use this knowledge to our advantage becuse we already that in the end of this function the this keyword will basically be returned and so whatever we add to that empty object will then be returned from the function and that returned object is gonna be the object that we are trying to build here:
   this.firstName = firstName; //instance properties
   this.birthYear = birthYear; //instance properties
-  //here we gave same name to the properties as of parameters ofcourse we can set other names but it's just a convention
-
-  //Never do this
-  //   this.calcAge = function () {
-  //     // console.log(2037 - this.birthYear);
-  //   };
-  //we should never create a method inside of a constructor function, it is because:
-  //image we were gonna create a hundered or thousands or even tens of thousands of person objects using this constructor function then what would happen is that each of these objects would carry around this function here so if we had a thousand objects we would essentially create a thousand copies of this function and so that would be terrible of the performance of our code but instead to solve this problem we are gonna use prototypes and prototype inheritance
 };
 
 const jonas = new Person("Jonas", 1991);
-
-// console.log(jonas);
-
-//Now we can use this constructor function to create as many different objects as we want:
 const matilda = new Person("Matilda", 2017);
 const jack = new Person("Jack", 2001);
-
-// console.log(matilda, jack);
-
-//And this is a bit like the analogy from before where the above constructor function is now the bluepring for a house and then each of these objects that we create through that function will be the actual house in the real world
-
-//Now remeber from one of the previous lectures that in classical OOP, an object created from a class is called an instance, now we didn't technically create a class here because as we discussed before javascript doesn't really have classes in the sense of traditional OOP however, we did created 3 objects from a constructor function. And constructor functions have been used since the beginning of javascript to kind of simulate classes and so therefore we can still say that jonas here is an instance of a Person and the same goes for Matilda and Jack and infact there is even an operator that we can use to test for that:
-// console.log(jonas instanceof Person); //true
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Start from here:
@@ -138,4 +113,3 @@ const h1 = document.querySelector("h1");
 //as we already know that all the DOM elements are behind the scenes are objects
 console.dir(h1);
 console.dir((x) => x + 1);
-
