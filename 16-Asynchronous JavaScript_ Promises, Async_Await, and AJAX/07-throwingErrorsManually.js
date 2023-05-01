c57798a7b2df0c1dae9e75e6177f8b2afb81bcd8
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-01 18:11:53 +0530

    implementing challenge

--- 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/07-throwingErrorsManually.js
+++ 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/07-throwingErrorsManually.js
@@ -99,22 +99,23 @@ const getCountryData = function (country) {
   getJSON(
     `https://restcountries.com/v3.1/name/${country}`,
     'Country not found '
-  ).then(data => {
+  )
+    .then(data => {
       renderCountry(data[0]);
 
-      const neighbour = 'dfkl';
+      //   const neighbour = 'dfkl';
+      const neighbour = data[0].borders?.[0];
 
-      if (!neighbour) return;
+      if (!neighbour) 
+      //for now neighbour country ex: australia
+      throw new Error('No neigbour found!');
 
-      return getJSON(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+      return getJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour}`,
+        'Country not found'
+      );
     })
-    .then(response => {
-      //Breaking DRY:
-      if (!response.ok)
-        throw new Error(`Country not found (${response.status})`);
-      //making a function, look above
-      return response.json();
-    })
+
     .then(data => renderCountry(data[0], 'neighbour'))
     .catch(err => {
       console.error(`${err} ⬇️⬇️⬇️`);
@@ -126,5 +127,5 @@ const getCountryData = function (country) {
 };
 
 btn.addEventListener('click', function () {
-  getCountryData('portugal');
+  getCountryData('australia');
 });
