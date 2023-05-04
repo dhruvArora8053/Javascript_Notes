c668865bf279f010dbafda2af5a58d266ebcba44
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-04 21:12:15 +0530

    implementing async

---
+++ 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/13-consumingPromisesWithAsyncAndAwait.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const whereAmI = async function(country){
+
+}
+//this function is now an asynchronous function so a function that will basically keep running in the background while performing the code that inside of it then when this function is done it automatically returns a promise 
