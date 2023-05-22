2156a2206a5f68868a41beab8afc40f85b2057f5
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-22 20:48:36 +0530

    displayed marker component

--- 15-Mapty App/script.js
+++ 15-Mapty App/script.js
@@ -37,9 +37,16 @@ if (navigator.geolocation) {
         console.log(mapEvent);
         const { lat, lng } = mapEvent.latlng;
  
-        L.marker(coords)
+        L.marker([lat, lng])
           .addTo(map)
-          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          .bindPopup(L.popup({
+            maxWidth: 250, 
+            minWidth: 100, 
+            autoClose: false, 
+            closeOnClick: false,
+            className: 'running-popup',
+          }))
+          .setPopupContent('Workout')
           .openPopup();
       });
     },
