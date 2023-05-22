03909c8a6346330514568351ca15f19ef628da46
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-22 20:21:22 +0530

    displaying a map marker

--- 15-Mapty App/script.js
+++ 15-Mapty App/script.js
@@ -26,16 +26,22 @@ if (navigator.geolocation) {
       const coords = [latitude, longitude];
 
       const map = L.map('map').setView(coords, 13);
+      console.log(map);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+ 
+        L.marker(coords)
+          .addTo(map)
+          .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position');
