f1b6198a8ddc91612c0ae425fc808e908e5b3523
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-02 17:05:39 +0530

    consuming promises recalled

--- 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/04-consumingPromises.js
+++ 16-Asynchronous JavaScript_ Promises, Async_Await, and AJAX/04-consumingPromises.js
@@ -21,8 +21,8 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-// //////////////////////////////////////////////////
-// //Start from here:-
+//////////////////////////////////////////////////
+//Start from here:-
 // // const request = new XMLHttpRequest();
 // // request.open('GET', 'https://restcountries.com/v3.1/name/portugal');
 // // request.send();
@@ -31,7 +31,7 @@ const renderCountry = function (data, className = '') {
 // console.log(request);
 
 // const getCountryData = function (country) {
-//   //and as we alredy know calling the fetch function like this will then immdediately return a promise so as soon as we start the request and in the beginning this promise is ofcourse still pending because the asynchronous task of getting the data is still running in the background now ofcourse at a certain point the promise will then be settled and either in a fulfilled or in a rejected state but for now let's assume success so assume that promise will be fulfilled and that we have a value available to work with so to handle this fullfilled state we can use the then method that is available on all promises:
+//   //and as we already know calling the fetch function like this will then immdediately return a promise so as soon as we start the request and in the beginning this promise is ofcourse still pending because the asynchronous task of getting the data is still running in the background now ofcourse at a certain point the promise will then be settled and either in a fulfilled or in a rejected state but for now let's assume success so assume that promise will be fulfilled and that we have a value available to work with so to handle this fullfilled state we can use the then method that is available on all promises:
 
 //   //now into the then method we need to pass a callback function that we want to be executed as soon as the promise is actually fulfilled so as soon as the result is available:
 //   //now this callback function will actually recieve one argument once it's called by javascript and that argument is the resulting value of the fulfilled promise:
