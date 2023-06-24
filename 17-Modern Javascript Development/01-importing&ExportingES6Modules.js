44a3ac565304b92978a2f381bbe7cac3909f8fc3
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-25 02:22:10 +0530

    implementing modules

--- 17-Modern Javascript Development/01-importing&ExportingES6Modules.js
+++ 17-Modern Javascript Development/01-importing&ExportingES6Modules.js
@@ -1,5 +1,13 @@
 //Importing Module
-import "./01-shoppingCart.js";
+import { addToCart, totalPrice, totalQuantity } from "./01-shoppingCart.js";
 // '.' means current location
 
 console.log("Importing Module");
+//so here in the console we see that the first log is actually exporting module only then importing module is logged to the console and so this means that infact this shoppingCart code here is executed before any code in the importing module so again the code in this module here is parsed and before it is executed all the code in the modules that it imports is executed first and the same is true if we have the log of "Importing Module" before. So remember all the importing statements are basically hoisted to the top.
+//And also not here we didn't use the strict mode and that's because all modules are executed in strict mode by default.
+
+// console.log(shippingCost); //error
+
+addToCart("bread", 5);
+console.log(totalPrice);
+console.log(totalQuantity);
--- 17-Modern Javascript Development/01-shoppingCart.js
+++ 17-Modern Javascript Development/01-shoppingCart.js
@@ -1,2 +1,25 @@
 //Exporting Module
-console.log('Exporting Module');
+console.log("Exporting Module");
+
+const shippingCost = 10;
+const cart = [];
+//now the variables that are declared inside of a module are actually scoped to this module so basically inside a module, the module itself is like the top level scope and so by default this means that all top level variables are private inside of this variable so unlike traditional scripts which would put all of these variables here in the global scope so that's we cannot log shippingCost or cart in other importing file
+
+//Exporting:
+export const addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+};
+
+//   if(true){
+//     export const addToCart = function (product, quantity) {
+//         cart.push({ product, quantity });
+//         console.log(`${quantity} ${product} added to cart`);
+//       };
+//   }
+//the export wouldn't work like this so it always have to be the top level code without any blocks
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity };
