767817d3e25b94c700772c972a11315454ad3edc
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-25 02:44:25 +0530

    implemented modules

--- 17-Modern Javascript Development/01-importing&ExportingES6Modules.js
+++ 17-Modern Javascript Development/01-importing&ExportingES6Modules.js
@@ -1,5 +1,5 @@
 //Importing Module
-import { addToCart, totalPrice, totalQuantity } from "./01-shoppingCart.js";
+// import { addToCart, totalPrice as price, quantity } from "./01-shoppingCart.js";
 // '.' means current location
 
 console.log("Importing Module");
@@ -8,6 +8,48 @@ console.log("Importing Module");
 
 // console.log(shippingCost); //error
 
-addToCart("bread", 5);
-console.log(totalPrice);
-console.log(totalQuantity);
+// addToCart("bread", 5);
+// console.log(price);
+// console.log(quantity);
+
+//importing all the exports of a module at the same time:
+// import * as ShoppingCart from './01-shoppingCart.js'
+// console.log(ShoppingCart);
+
+// ShoppingCart.addToCart('bread', 5)
+// console.log(ShoppingCart.totalPrice);
+//it looks pretty similar to classes and we can also say that a module export kind of an public API because everything else ofcourse stays private inside of a module
+
+//Default import:
+// import add, {
+//   addToCart,
+//   totalPrice as price,
+// } from "./01-shoppingCart.js";
+
+// add("pizza", 2);
+// console.log(price);
+//we should not mix named and default exports like we did here above to avoid the complexity
+
+// import add from "./01-shoppingCart.js";
+
+// add("pizza", 2);
+// console.log(price);
+//so the prffered style is actually to just use one default export per module and then import that here like we did
+
+//again we should not do this
+import add, { cart } from "./01-shoppingCart.js";
+add("pizza", 2);
+add("bread", 5);
+add("apples", 4);
+
+console.log(cart);
+//imports are not the copy of exports, they are instead like a live connection and so what that means is I point to the same place in a memeory because otherwise if it was a copy then here we would not get anything in the array
+
+//My Example:-
+// const bike = [];
+// const bikeDetails= function(brand, model){
+//     bike.push({brand, model})
+// }
+
+// bikeDetails('bajaj', 'pulsar')
+// console.log(bike);
--- 17-Modern Javascript Development/01-shoppingCart.js
+++ 17-Modern Javascript Development/01-shoppingCart.js
@@ -2,7 +2,7 @@
 console.log("Exporting Module");
 
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 //now the variables that are declared inside of a module are actually scoped to this module so basically inside a module, the module itself is like the top level scope and so by default this means that all top level variables are private inside of this variable so unlike traditional scripts which would put all of these variables here in the global scope so that's we cannot log shippingCost or cart in other importing file
 
 //Exporting:
@@ -22,4 +22,11 @@ export const addToCart = function (product, quantity) {
 const totalPrice = 237;
 const totalQuantity = 23;
 
-export { totalPrice, totalQuantity };
+export { totalPrice, totalQuantity as quantity};
+
+//Default Exports:
+export default function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
